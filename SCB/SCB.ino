52f891ef75b2c4e3abb729afc63d20ff8a9b9d5a
Emiliano Enríquez <129916866+WeroKikez@users.noreply.github.com>
2024-11-02 21:04:16 -0600

    Version 2
    General features working; needs 3 signals

--- SCB/SCB.ino
+++ SCB/SCB.ino
@@ -1,56 +1,143 @@
 #include <Servo.h>
 
 Servo ballSorter;
+Servo gate;
+
+// Servo Base Positions
 int home = 95;
 int left = home + 60;
-int right = home - 55;
+int right = home - 52;
 
+// Gate Servo Base Positions
+int closed = 83;
+int openedLeft = 98;
+int openedRight = 68;
+
+// Activators & Pins
+int infraredSensorPin = 12;
+int gateOpenLeftActivatorPin = 4;
+int gateOpenRightActivatorPin = 5;
+int sortingActivatorPin = 10;
+
+// Gate Movement
+int gatePos;
+int gateSpeed = 30;
+int gateClosingSpeed = 40;
+
+// Sorting movement
+int sortPos;
+int sortDelay = 300;
+int waitForNextDelay = 400;
+
+
+// Global State and General Settings
+int gateState = 0; // 0: Closed; 1: Open Left; 2: Open Right
 bool sortingActivated = false;
-int sortDelay = 1000;
+int iteration = 2;
+int delayTime = 300;
+// TODO: When receiving a signal, keep the circuit working. 
+// Connect power between boards and use a transistor to send signal from ESP32
 
 void setup() {
   Serial.begin(9600);
-  pinMode(5 , INPUT);
+  pinMode(1, OUTPUT);
+  pinMode(infraredSensorPin, INPUT); // Infrared sensor
 
+  // Sorter Setup
   ballSorter.attach(8);
   ballSorter.write(home);
-  delay(5000);
+  sortPos = home;
+
+  // Gate Setup
+  gate.attach(7);
+  gate.write(closed);
+  gatePos = closed;
+
+  Serial.println("===================");
+  delay(4000);
 }
 
 void loop() {
-  if(Serial.available() == 0) {
-    int activate = Serial.parseInt();
-    switch (activate) {
-      case 1:
-        sortingActivated = true;
-        Serial.println("Active");
-        break;
+  digitalWrite(1, HIGH);
 
-      case 2:
-        sortingActivated = false;
-        ballSorter.write(home);
-        break;
-    }
-  }
+  Serial.print("Sort Activator: ");
+  Serial.println(digitalRead(sortingActivatorPin));
+  Serial.print("Left Activator: ");
+  Serial.println(digitalRead(gateOpenLeftActivatorPin));
+  Serial.print("Right Activator: ");
+  Serial.println(digitalRead(gateOpenRightActivatorPin));
+  Serial.println("---------------------");
+  
+  
+  
+  sortingActivated = digitalRead(sortingActivatorPin);
 
-  if(sortingActivated) {
-    int isBlack = 0;
-    isBlack = digitalRead(5);
+  if(sortingActivated) { // Sorting Logic
+    int isBlack;
+    isBlack = digitalRead(infraredSensorPin);
+
+    /* Serial.println();
+    Serial.print("Black: ");
+    Serial.print(isBlack);
+    Serial.println(); */
 
     if(isBlack) {
       ballSorter.write(left);
+      sortPos = left;
       Serial.print("Servo at: ");
-      Serial.println(left);
+      Serial.println(sortPos);
     } else {
       ballSorter.write(right);
+      sortPos = right;
       Serial.print("Servo at: ");
-      Serial.println(right);
+      Serial.println(sortPos);
     }
 
     delay(sortDelay);
-
     ballSorter.write(home);
+    delay(waitForNextDelay);
+  } else { // Gate Logic
+    if(digitalRead(gateOpenLeftActivatorPin)) {
+      gateState = 1;
+    } else if(digitalRead(gateOpenRightActivatorPin)) {
+      gateState = 2;
+    } else {
+      gateState = 0;
+    }
+
+    switch (gateState) {
+      case 0: 
+        moveGate(closed, 60);
+        break;
+      case 1: 
+        moveGate(openedLeft, 40);
+        break;
+      case 2: 
+        moveGate(openedRight, 40);
+        break;
+    }
   }
 
-  delay(2000);
+  delay(delayTime);
+}
+
+void moveGate(int target, int speed) {
+  if(gatePos == target) {
+    Serial.println("We're here, not moving");
+    return;
+  }
+
+  if(target > gatePos) { // left
+    for(int pos = gatePos; pos <= target; pos++) {
+      gate.write(pos);
+      delay(speed);
+    }
+  } else { // right
+    for(int pos = gatePos; pos >= target; pos--) {
+      gate.write(pos);
+      delay(speed);
+    }
+  }
+
+  gatePos = target;
 }
